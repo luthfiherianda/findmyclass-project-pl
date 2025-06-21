6586b9b8a3388c6704c4b0c6a07399327064c8bf
azrielfarelo <azrielefarelo@gmail.com>
2025-06-21 20:47:41 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-// Ambil elemen-elemen yang dibutuhkan dari DOM
-const menuBtn = document.getElementById('menu-btn');
-const closeBtn = document.getElementById('close-btn');
-const sidebar = document.getElementById('sidebar');
-const overlay = document.getElementById('overlay');
+function toggleVisibility(inputId, iconElement) {
+  const input = document.getElementById(inputId);
+  const isPassword = input.type === "password";
 
-// Fungsi untuk membuka sidebar
-function openSidebar() {
-    sidebar.classList.add('show');
-    overlay.classList.add('show');
+  input.type = isPassword ? "text" : "password";
+
+  iconElement.innerHTML = isPassword ? '<i class="fas fa-eye-slash"></i>' : '<i class="fas fa-eye"></i>';
 }
-
-// Fungsi untuk menutup sidebar
-function closeSidebar() {
-    sidebar.classList.remove('show');
-    overlay.classList.remove('show');
-}
-
-// Tambahkan event listener untuk tombol menu
-menuBtn.addEventListener('click', openSidebar);
-
-// Tambahkan event listener untuk tombol tutup
-closeBtn.addEventListener('click', closeSidebar);
-
-// Tambahkan event listener untuk overlay (menutup sidebar saat area gelap diklik)
-overlay.addEventListener('click', closeSidebar);
-
-function toggleSubmenu(event) {
-  event.preventDefault();
-  const parent = event.target.closest('li');
-  parent.classList.toggle('open');
-}
-
