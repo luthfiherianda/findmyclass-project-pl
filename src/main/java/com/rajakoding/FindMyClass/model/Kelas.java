2e194dcf2b2ac42a585abdbfce0b2bee61e082e3
luthfiherianda <luthfiherianda@gmail.com>
2025-06-27 19:36:31 +0700

    Initial commit

---
+++ src/main/java/com/rajakoding/FindMyClass/model/Kelas.java
@@ -0,0 +1,24 @@
+package com.rajakoding.FindMyClass.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "kelas")
+public class Kelas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ruang;
+    private String kapasitas;
+
+    @ElementCollection
+    private List<String> fasilitas;
+
+    private String deskripsi;
+}
