5fff942a61df793c0748a36b8903e87983fc9481
luthfiherianda <luthfiherianda@gmail.com>
2025-07-07 01:09:11 +0700

    Hapus semua isi lama untuk upload versi baru

--- FINDMYCLASS 2.7/all3 - Copy (6)/all3 - Copy/lombok.java
+++
@@ -1,9 +0,0 @@
-import lombok.Getter;
-import lombok.Setter;
-
-public class TestLombok {
-    @Getter @Setter
-    private String name;
-}
-// Lombok will generate the getter and setter methods for the 'name' field
-// You can use the generated methods like this: 
