2450c8e2d8c314f8180bc6614d12a313e9c89577
luthfiherianda <luthfiherianda@gmail.com>
2025-07-06 23:54:16 +0700

    Upload ulang file terbaru

--- Backend_UPDATED/src/main/java/com/rajakoding/FindMyClass/repository/KelasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rajakoding.FindMyClass.repository;
-
-import com.rajakoding.FindMyClass.model.Kelas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KelasRepository extends JpaRepository<Kelas, Long> {
-}
--- src/main/java/com/rajakoding/FindMyClass/repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rajakoding.FindMyClass.repository;
-
-import com.rajakoding.FindMyClass.model.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
