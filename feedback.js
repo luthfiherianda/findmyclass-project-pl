ab6702c1ed83e491f3c8b1a3afaea75b106f7d6e
luthfiherianda <luthfiherianda@gmail.com>
2025-07-07 00:19:01 +0700

    Upload ulang isi proyek terbaru

--- feedback.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('feedbackForm').addEventListener('submit', function(e) {
-  e.preventDefault(); // Mencegah reload
-
-  // Simulasi submit (misalnya nanti ke backend / Google Form / DB)
-  document.getElementById('feedbackForm').reset();
-  document.getElementById('feedbackSuccess').classList.remove('hidden');
-});
-  
